0a7adc2ab7046eebd929b7cff443ec00cbf353d9
Samir Kassen <151970819+SiqSamir@users.noreply.github.com>
2026-04-30 12:40:14 -0300

    Delete SIVA directory

--- SIVA/src/main/java/com/ipem/api/modules/service/dto/RefuelingRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ipem.api.modules.service.dto;
-
-public record RefuelingRequestDTO(
-        Float liters,
-        Double pricePerLiter,
-        String invoice,
-        Boolean hadOilChange
-) {
-}
--- SIVA/src/main/java/com/ipem/api/modules/service/model/enums/RecordType.java
+++
@@ -1,8 +0,0 @@
-package com.ipem.api.modules.service.model.enums;
-
-public enum RecordType {
-    CHECK_IN,
-    CHECK_OUT,
-    REFUELING,
-    INCIDENT
-}
